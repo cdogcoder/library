a554e4164dbec757254eb44dc628d510b07a812b
cdog <cdog97456@gmail.com>
2025-02-06 10:08:02 -0800

    Create addBookToLibrary function to create and store book entries

--- script.js
+++ script.js
@@ -25,4 +25,31 @@ newBookLog.appendChild(newBookTitle)
 newBookLog.appendChild(newBookAuthor)
 newBookLog.appendChild(newBookPages)
 newBookLog.appendChild(newBookReadStatus)
-libraryTable.appendChild(newBookLog)
+books.push(newBookLog)
+
+function addBookToLibrary(bookTitle, author, pages, readStatus) {
+    const bookEntry = new Book(bookTitle, author, pages, readStatus);
+    
+    const newBookTitle = document.createElement("td");
+    const newBookAuthor = document.createElement("td");
+    const newBookPages = document.createElement("td");
+    const newBookReadStatus = document.createElement("td");
+
+    newBookTitle.textContent = bookEntry.bookTitle;
+    newBookAuthor.textContent = bookEntry.author;
+    newBookPages.textContent = bookEntry.pages;
+    newBookReadStatus.textContent = bookEntry.readStatus;
+
+    const newBookLog = document.createElement("tr");
+    newBookLog.appendChild(newBookTitle);
+    newBookLog.appendChild(newBookAuthor);
+    newBookLog.appendChild(newBookPages);
+    newBookLog.appendChild(newBookReadStatus);
+    books.push(newBookLog);
+    
+}
+
+addBookToLibrary("Harry Potter and the Chamber of Secrets", "JK Rowling", 200, "Read")
+for (book of books) {
+    libraryTable.appendChild(book);
+}
