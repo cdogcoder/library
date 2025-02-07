f942ebdb2350f9699a0974339cf20ac4a693d1da
cdogcoder <cdogcoder@gmail.com>
2025-02-06 21:02:55 -0800

    Add functionality to close dialog upon backdrop click
    Credits to Web Dev Simplified on YouTube (view
    README.md for related links)

--- script.js
+++ script.js
@@ -61,3 +61,15 @@ addBookEntryButton.addEventListener("click", () => {
 cancelBookEntryButton.addEventListener("click", () => {
     dialog.close();
 })
+
+dialog.addEventListener("click", (e) => {
+    const dialogDimensions = dialog.getBoundingClientRect();
+    if (
+        e.clientX < dialogDimensions.left ||
+        e.clientX > dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY > dialogDimensions.bottom
+    ) {
+        dialog.close();
+    }
+})
